f68e14033d46fa451a6ed5a43c1fc5fd0885161c
Sage <3365174583@qq.com>
2026-03-13 09:42:00 +0800

    hw1: 添加清屏

--- homework_1/src/main.py
+++ homework_1/src/main.py
@@ -1,9 +1,19 @@
 # 主程序入口
 from utils import print_menu
-from student_manager import add_student, search_student, modify_student, delete_student, show_all_students
+from student_manager import (
+    add_student,
+    search_student,
+    modify_student,
+    delete_student,
+    show_all_students,
+)
+import os   # TODO: 导入os模块，用于清屏操作
 
 def main():
     while True:
+        # 清屏，确保屏幕上只显示当前菜单
+        os.system("cls" if os.name == "nt" else "clear")
+        
         # 打印菜单
         print_menu()
 
@@ -11,18 +21,18 @@ def main():
         choice = input("请输入您的选择 (0-5): ").strip()
 
         # 处理用户选择
-        if choice == '0':
+        if choice == "0":
             print("退出系统，再见！")
             break
-        elif choice == '1':
+        elif choice == "1":
             add_student()
-        elif choice == '2':
+        elif choice == "2":
             search_student()
-        elif choice == '3':
+        elif choice == "3":
             modify_student()
-        elif choice == '4':
+        elif choice == "4":
             delete_student()
-        elif choice == '5':
+        elif choice == "5":
             show_all_students()
         else:
             print("输入不合法，请重新选择")
